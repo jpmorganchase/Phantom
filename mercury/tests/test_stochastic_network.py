ddb53cf83678ed1be661b689ade6c35273592127
Leo Ardon <leo.ardon.la@gmail.com>
2021-11-16 16:23:21 +0000

    fix[mercury]: stochastic network rate parameter preserved (#25)
    * fix[mercury]: stochastic network rate parameter preserved
    
    * chore[mercury]: black format
    
    * fix(me): make rate parameter in mercury stochastic network more explicit

--- mercury/tests/test_stochastic_network.py
+++ mercury/tests/test_stochastic_network.py
@@ -9,10 +9,12 @@ def test_stochastic_network_1():
     net.add_connection("A", "B", 1.0)
 
     assert net.graph.has_edge("A", "B")
+    assert net.graph.has_edge("B", "A")
 
     net.resample_connectivity()
 
     assert net.graph.has_edge("A", "B")
+    assert net.graph.has_edge("B", "A")
 
 
 def test_stochastic_network_2():
@@ -31,6 +33,18 @@ def test_stochastic_network_3():
     net = StochasticNetwork(UnorderedResolver(), [Actor("A"), Actor("B")])
 
     net.add_connections_from([("A", "B", 0.0)])
+
+    assert not net.graph.has_edge("A", "B")
+
+    net.resample_connectivity()
+
+    assert not net.graph.has_edge("A", "B")
+
+
+def test_stochastic_network_4():
+    net = StochasticNetwork(UnorderedResolver(), [Actor("A"), Actor("B")])
+
+    net.add_connections_between(["A"], ["B"], rate=0.0)
 
     assert not net.graph.has_edge("A", "B")
 
