71b74dbf7a8474380ec1511cf158614f949e367c
Leo Ardon <leo.ardon.la@gmail.com>
2022-02-02 18:09:04 +0000

    feat: add support for network connectivity sampling (#53)
    * feat: comparable sampler
    
    * feat: support for sampled connectivity

--- phantom/phantom/utils/samplers.py
+++ phantom/phantom/utils/samplers.py
@@ -1,10 +1,51 @@
+import functools
 from abc import ABC, abstractmethod
-from typing import Generic, Iterable, Optional, Tuple, TypeVar
+from typing import (
+    Callable,
+    Generic,
+    Iterable,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 import numpy as np
 
-
 T = TypeVar("T")
+
+
+class ComparableType(Generic[T], ABC):
+    """
+    Interface for Types that can be compared.
+    """
+
+    @abstractmethod
+    def __lt__(self, other: T) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __le__(self, other: T) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __gt__(self, other: T) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __ge__(self, other: T) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __eq__(self, other: object) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __ne__(self, other: object) -> bool:
+        raise NotImplementedError
+
+
+ComparableT = TypeVar("ComparableT", bound=ComparableType)
 
 
 class BaseSampler(ABC, Generic[T]):
@@ -23,7 +64,42 @@ class BaseSampler(ABC, Generic[T]):
         raise NotImplementedError
 
 
-class UniformSampler(BaseSampler[float]):
+class ComparableSampler(BaseSampler[ComparableT], Generic[ComparableT]):
+    """
+    Extension of the `BaseSampler` for ComparableTypes in order
+    to treat the `ComparableSampler` like its actual internal value.
+
+    Example:
+    >>> s = UniformSampler()
+    >>> s.value = s.sample()
+    >>> s <= 1.0
+    # True
+    >>> s == 1.5
+    # False
+    """
+
+    def __lt__(self, other: Union[ComparableT, "ComparableSampler"]) -> bool:
+        if self.value is None:
+            raise ValueError("`self.value` is None")
+        return self.value < other
+
+    def __eq__(self, other: object) -> bool:
+        return self.value == other
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+    def __le__(self, other: Union[ComparableT, "ComparableSampler"]) -> bool:
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other: Union[ComparableT, "ComparableSampler"]) -> bool:
+        return not self.__le__(other)
+
+    def __ge__(self, other: Union[ComparableT, "ComparableSampler"]) -> bool:
+        return self.__gt__(other) or self.__eq__(other)
+
+
+class UniformSampler(ComparableSampler[float]):
     """
     Samples a single float value from a uniform distribution.
     """
@@ -51,7 +127,7 @@ class UniformSampler(BaseSampler[float]):
         return value
 
 
-class UniformArraySampler(BaseSampler[np.ndarray]):
+class UniformArraySampler(ComparableSampler[np.ndarray]):
     """
     Samples an array of float values from a uniform distribution.
     """
@@ -81,7 +157,7 @@ class UniformArraySampler(BaseSampler[np.ndarray]):
         return value
 
 
-class NormalSampler(BaseSampler[float]):
+class NormalSampler(ComparableSampler[float]):
     """
     Samples a single float value from a normal distribution.
     """
@@ -109,7 +185,7 @@ class NormalSampler(BaseSampler[float]):
         return value
 
 
-class NormalArraySampler(BaseSampler[np.ndarray]):
+class NormalArraySampler(ComparableSampler[np.ndarray]):
     """
     Samples an array of float values from a normal distribution.
     """
@@ -137,3 +213,22 @@ class NormalArraySampler(BaseSampler[np.ndarray]):
             value = np.clip(value, self.clip_low, self.clip_high)
 
         return value
+
+
+class LambdaSampler(BaseSampler[T]):
+    """
+    Samples using an arbitrary lambda function
+    """
+
+    def __init__(self, *args, func: Callable[..., T] = None, **kwargs):
+        if func is None:
+            raise ValueError("You must provide a `func`")
+
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+        super().__init__()
+
+    def sample(self) -> T:
+        return self.func(*self.args, **self.kwargs)
