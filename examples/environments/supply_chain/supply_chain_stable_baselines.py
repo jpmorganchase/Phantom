2c8ac55c87656cb3938ecfd4e257add9f5b6a4a2
Jared Vann <jared@vann.me>
2022-08-15 09:52:55 +0000

    fix: fix GitHub Actions, apply Black formatter

--- examples/environments/supply_chain/supply_chain_stable_baselines.py
+++ examples/environments/supply_chain/supply_chain_stable_baselines.py
@@ -243,7 +243,7 @@ env = ph.SingleAgentEnvAdapter(
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./tensorboard/")
 model.learn(total_timesteps=1e7)
 model.save(PATH)
-#model = PPO.load(PATH, env=env) loading pre-trained model
+# model = PPO.load(PATH, env=env) loading pre-trained model
 
 obs = env.reset()
 for i in range(100):
