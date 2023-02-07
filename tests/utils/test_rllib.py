94732ef25dda8e5df94ae1dec52a8f218d6a893b
Jared Vann <jared@vann.me>
2023-02-07 14:48:42 +0000

    fix(ph): try to fix github tests

--- tests/utils/test_rllib.py
+++ tests/utils/test_rllib.py
@@ -1,4 +1,3 @@
-import numpy as np
 import phantom as ph
 import pytest
 
@@ -56,7 +55,7 @@ def test_rllib_train_rollout(tmpdir):
     # Evaluate policy:
     results = ph.utils.rllib.evaluate_policy(
         directory=f"{tmpdir}/LATEST",
-        obs=ph.utils.ranges.LinspaceRange(0, 1, 2, name="r", dtype=np.int),
+        obs=ph.utils.ranges.LinspaceRange(0, 1, 2, name="r", dtype=int),
         policy_id="mock_policy",
     )
     assert list(results) == [({"r": 0.0}, 0.0, 0), ({"r": 1.0}, 1.0, 0)]
