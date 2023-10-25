41e4a6a4b9e52ce7d43c243afee4f295c69c5cdc
Jared Vann <jared@vann.me>
2023-10-25 08:26:27 +0100

    Add more payload check unit tests

--- tests/network/test_payload_checks.py
+++ tests/network/test_payload_checks.py
@@ -11,16 +11,6 @@ class AgentB(ph.Agent):
     pass
 
 
-@ph.msg_payload()
-class Payload1:
-    pass
-
-
-@ph.msg_payload(AgentA, AgentB)
-class Payload2:
-    pass
-
-
 def test_payload_checks():
     agents = [
         AgentA("A"),
@@ -30,10 +20,36 @@ def test_payload_checks():
     net = ph.Network(agents, enforce_msg_payload_checks=True)
     net.add_connection("A", "B")
 
+    @ph.msg_payload()
+    class Payload1:
+        pass
+
     net.send("A", "B", Payload1())
     net.send("B", "A", Payload1())
 
+    @ph.msg_payload(AgentA, AgentB)
+    class Payload2:
+        pass
+
     net.send("A", "B", Payload2())
+
+    @ph.msg_payload([AgentA, AgentB], [AgentA, AgentB])
+    class Payload3:
+        pass
+
+    net.send("A", "B", Payload3())
+    net.send("B", "A", Payload3())
+
+    @ph.msg_payload(sender_type=AgentA, receiver_type=None)
+    class Payload4:
+        pass
+
+    @ph.msg_payload(sender_type=None, receiver_type=AgentA)
+    class Payload5:
+        pass
+
+    net.send("A", "B", Payload4())
+    net.send("B", "A", Payload5())
 
     with pytest.raises(ph.network.NetworkError):
         net.send("B", "A", Payload2())
