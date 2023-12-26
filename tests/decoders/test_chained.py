2bb7ece6302ceeeaa52e5b9582166ccf5b50f6bd
Nathan D'Souza <nathandsouza10@gmail.com>
2023-12-26 16:05:58 +0000

    Add tests for chain method improvements in encoders and decoders
    Included new unit tests to validate the independence and functionality of the improved chain methods in ChainedEncoder and ChainedDecoder classes.

--- tests/decoders/test_chained.py
+++ tests/decoders/test_chained.py
@@ -49,3 +49,24 @@ def test_chained_decoder_reset():
 
     assert d1.id is None
     assert d2.id is None
+
+
+def test_chained_decoder_independence():
+    d1 = SimpleDecoder(1)
+    d2 = SimpleDecoder(2)
+    d3 = SimpleDecoder(3)
+
+    cd1 = ChainedDecoder([d1, d2])
+
+    cd2 = cd1.chain([d3])
+
+    messages_cd2 = cd2.decode(None, [None, None, None])
+    assert messages_cd2 == [
+        ("RECIPIENT", "FROM 1"),
+        ("RECIPIENT", "FROM 2"),
+        ("RECIPIENT", "FROM 3"),
+    ]
+
+    messages_cd1 = cd1.decode(None, [None, None])
+    assert messages_cd1 == [("RECIPIENT", "FROM 1"), ("RECIPIENT", "FROM 2")]
+    assert len(messages_cd1) == 2
